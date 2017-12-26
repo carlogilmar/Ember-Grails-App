1de441c9a3e32def39b6740f645348f9b6cf8b65
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-26 10:11:32 -0600

    Agregando app de grails

---
+++ myapp/grails-app/init/myapp/BootStrap.groovy
@@ -0,0 +1,17 @@
+package myapp
+
+class BootStrap {
+
+    def init = { servletContext ->
+        def todos = [
+                [name: 'clean the house'],
+                [name: 'water the dog'],
+                [name: 'feed the lawn'],
+                [name: 'pay dem bills'],
+                [name: 'run'],
+                [name: 'swim']
+        ].each { new Todo(name: it.name).save() }
+    }
+    def destroy = {
+    }
+}
